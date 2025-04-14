5c44207e0998c33bc25e28b762643b9ecb20b506
hdbr00 <h_ledezma506_08_12@outlook.com>
2025-04-13 19:07:32 -0600

    Add Http Request handler interface

---
+++ src/IRequestHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_http_server.src
+{
+    interface IRequestHandler
+    {
+        HttpResponse Handle(HttpRequest request, string directory);
+    }
+}
