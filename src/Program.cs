d564ed8660251a09d857faab053db43ddec21774
hdbr00 <h_ledezma506_08_12@outlook.com>
2025-04-13 19:18:31 -0600

    Add server entry point

---
+++ src/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codecrafters_http_server.src
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Logs from your program will appear here!");
+
+            string directory = GetDirectoryFromArgs(args);
+            var server = new HttpServer(4221, directory);
+            server.Start();
+        }
+
+        private static string GetDirectoryFromArgs(string[] args)
+        {
+            for (int i = 0; i < args.Length - 1; i++)
+            {
+                if (args[i] == "--directory")
+                {
+                    return args[i + 1];
+                }
+            }
+            return Directory.GetCurrentDirectory();
+        }
+    }
+}
